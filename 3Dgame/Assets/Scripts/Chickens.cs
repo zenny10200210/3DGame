e06adf4ad60396d6a04fec03c30c5d43895f0338
zenny10200210 <55470595+zenny10200210@users.noreply.github.com>
2019-10-30 15:56:43 +0800

    Chicken8

---
+++ 3Dgame/Assets/Scripts/Chickens.cs
@@ -0,0 +1,12 @@
+﻿using UnityEngine; //引用Unity  API
+
+//      類別   類別名稱要等於檔案名稱
+public class Chickens : MonoBehaviour
+{
+    //宣告變數(定義欄位 Field)
+    //欄位類型 欄位名稱  指定 值 結束
+    int speed = 10; //變數1,999,-100
+    float turn = 20.5f;
+    bool mission;
+    string name = "G8雞";
+}

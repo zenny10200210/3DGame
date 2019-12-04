711dfe4b88e89e15564f2634efc5ba880d5d8aff
zenny10200210 <55470595+zenny10200210@users.noreply.github.com>
2019-12-04 15:57:21 +0800

    運算子

---
+++ 3Dgame/Assets/Scripts/LearnIf.cs
@@ -0,0 +1,64 @@
+﻿using UnityEngine;
+
+public class LearnIf : MonoBehaviour
+{
+    public bool openDoor;
+    public int score;
+    public int hit;
+    private void Start()
+    {
+        // 語法：if (布林值) { 陳述式或演算法 }
+        // () 內的布林值為 true 執行 { }
+        if (true)
+        {
+            print("測試!");
+        }
+    }
+
+    private void Update()
+    {
+        if (openDoor)
+        {
+            //if()內的布林值為true執行if {}
+            print("開門");
+        }
+        else
+        {
+            //if()內的布林值為false執行if {}
+            print("關門");
+        }
+        if (score >= 60)
+        {
+            print("及格");
+        }
+        else if(score >= 50)
+        {
+            print("可以補考");
+        }
+        else if (score >= 40)
+        {
+            print("可以付錢補考==+");
+        }
+        else
+        {
+            print("被當惹");
+        }
+
+        if (hit >= 150)
+        {
+            print("攻擊力10倍");
+        }
+        else if (hit >= 100&& hit<150)
+        {
+            print("攻擊力5倍");
+        }
+        else if (hit >= 50 && hit < 100)
+        {
+            print("攻擊力2倍");
+        }
+        else
+        {
+        }
+
+    }
+}
---
+++ 3Dgame/Assets/Scripts/LearnOperator.cs
@@ -0,0 +1,69 @@
+﻿using UnityEngine;
+
+public class LearnOperator : MonoBehaviour
+{
+    public int A = 10, B = 3;
+    public int C = 1;
+    public int D = 1, E = 1;
+    public int F = 10;
+
+    public float G = 1.5f, H = 99.9f;
+    public float hp = 50;
+    public bool key ;
+    public int enemy;
+    private void Start()
+    {
+        #region 數學運算子
+        print(A + B);
+        print(A - B);
+        print(A * B);
+        print(A / B);
+        print(A % B);
+      
+        //指派右邊先運算再給左邊
+        C = 99 + 1;
+        print(C);
+        C = C + 1; //原始寫法
+        //遞增、遞減 + -
+        print(D++); //後置遞增：先輸出再運算 - 看到 1 電腦內 2
+        print(++E); //前置遞增：先運算再輸出 - 看到 2 電腦內 2
+
+        //加法指派 - * / %
+        //F=F+100;
+        F+= 100;
+        print(F);
+        #endregion
+
+        #region 比較運算子
+        print(G > H); //false
+        print(G < H); //true
+        print(G >= H); //false
+        print(G <= H); //true
+        print(G == H); //false
+        print(G != H); //true
+        #endregion
+
+        #region 邏輯運算子
+        //並且 && 有一個 false 結果就是 false
+        print(true && true); //true
+        print(true && false); //false
+        print(false && true); //false
+        print(false && false); //true
+
+        //或者 | | 有一個 true 結果就是 true
+        print(true || true); //true
+        print(true || false); //true
+        print(false || true); //true
+        print(false || false); //false
+
+        //相反 (顛倒)
+        print(!true); //false
+        print(!false); //true
+        #endregion
+    }
+    private void Update()
+    {
+        print("死亡："+(hp<=0));
+        print("過關：" + (key || enemy>=5));
+    }
+}

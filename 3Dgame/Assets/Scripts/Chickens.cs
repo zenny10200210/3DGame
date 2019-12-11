a5f59346745873619e66887f9d1b29432615a9da
zenny10200210 <55470595+zenny10200210@users.noreply.github.com>
2019-12-11 15:57:18 +0800

    走路

--- 3Dgame/Assets/Scripts/Chickens.cs
+++ 3Dgame/Assets/Scripts/Chickens.cs
@@ -8,42 +8,60 @@ public class Chickens : MonoBehaviour
     //欄位類型 欄位名稱  (指定 值) 結束
     //私人 - 隱藏 private (預設)
     //公開 - 顯示 public
-    [Header("移動速度")][Range(1,100)]
+    [Header("移動速度")][Range(1,2000)]
     public int speed = 10;         //整數1,999,-100
-    [Tooltip("G8雞的旋轉速度"),Range(1.5f,200f)]
+    [Header("旋轉速度"),Tooltip("G8雞的旋轉速度"),Range(1.5f,200f)]
     public float turn = 20.5f;     // 浮點數
+    [Header("是否完成任務")]
     public bool mission;           //布林值 true false
+    [Header("玩家名稱")]
     public string _name = "G8雞";   //字串""
     #endregion
+
+    public Transform tran;
+    public Rigidbody rig;
+    public Animator ani;
+
+    private void Update()
+    {
+        Turn();
+        Run();
+    }
 
     #region 方法區域
     /// <summary>
     /// 跑步
     /// </summary>
     private void Run() 
-    { 
-
+    {
+        float v = Input.GetAxis("Vertical"); //W 上 1、S 下 -1、沒按 0
+        //rig.AddForce(0,0 ,speed * v)
+        //tran.forward 區域座標Z軸
+        //tran.right   區域座標X軸
+        //tran.up      區域座標Y軸
+        rig.AddForce(tran.forward * speed * v * Time.deltaTime);
     }
     /// <summary>
     /// 旋轉
     /// </summary>
     private void Turn()
     {
-
+        float h = Input.GetAxis("Horizontal"); //A 左 -1、D 右 1、沒按 0
+        tran.Rotate(0, turn * h * Time.deltaTime, 0);
     }
     /// <summary>
     /// 亂叫
     /// </summary>
     private void Bark()
     {
-
+        //按下空白鍵拍翅膀
     }
     /// <summary>
     /// 撿東西
     /// </summary>
     private void Catch()
     {
-
+        //按下左鍵撿東西
     }
     /// <summary>
     /// 檢視任務
---
+++ 3Dgame/Assets/Scripts/LearAPI.cs
@@ -0,0 +1,28 @@
+﻿using UnityEngine;
+
+public class LearAPI : MonoBehaviour
+{
+    //非靜態
+    //變形、剛體、動畫、音效、燈光、攝影機
+    //先定義蘭為存放並設定代號
+    //修飾詞 類型 名稱(代號)
+    public Transform test1;
+    public Transform test2;
+    void Start()
+    {
+     //取得非靜態屬性
+     //代號.屬性
+     print(test1.position);
+        //設定非靜態屬性
+        //代號.屬性 = 對應的類型
+        test1.localScale = new Vector3(10, 10, 10);
+        //new Vector3(1, 1, 1)*5;
+        test2.localScale = Vector3.one*5;
+    }
+    private void Update()
+    {
+        //代號.方法(對應引數)
+        test1.Rotate(0, 5, 0);
+        test2.Translate(0, 0, 1.5f);
+    }
+}
